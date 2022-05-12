5dd81a6cd242fb075f52ac15ff62826a46bb9491
ramc9777 <ramc9777@gmail.com>
2022-05-12 13:27:55 +0530

    firstCode

---
+++ java_basics/c1_about_java/firstCode.java
@@ -0,0 +1,9 @@
+package java_basics.c1_about_java;
+
+public class firstCode {
+    public static void main(String args[]){
+        System.out.println("Hello World");
+    }
+}
+
+//output: Hello World 
